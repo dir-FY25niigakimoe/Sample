135d28aeb24496703b1cabffde687af61104f3ae
dir-FY25niigakimoe <moe.niigaki.a389@dirbato.co.jp>
2025-05-23 11:54:17 +0900

    ソースコード１

--- Sample/src/Sample/Test.java
+++ Sample/src/Sample/Test.java
@@ -3,7 +3,9 @@ package Sample;
 public class Test {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        System.out.println("Kiramex"); 
+        System.out.println("Java"); 
+        System.out.println("学習を始めましょう!");
 
     }
 
