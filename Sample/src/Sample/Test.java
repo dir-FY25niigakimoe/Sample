9bc48b38359c78e65dec2568ccdf11936e0faea9
dir-FY25niigakimoe <moe.niigaki.a389@dirbato.co.jp>
2025-05-23 11:54:39 +0900

    コメントアウトの練習

--- Sample/src/Sample/Test.java
+++ Sample/src/Sample/Test.java
@@ -1,11 +1,13 @@
 package Sample;
 
 public class Test {
-
+    //メインメソッドはプログラムの最初に動作します。
     public static void main(String[] args) {
+        /*
         System.out.println("Kiramex"); 
         System.out.println("Java"); 
         System.out.println("学習を始めましょう!");
+        */
 
     }
 
