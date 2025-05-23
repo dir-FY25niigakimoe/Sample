b5aac6eec026966a791092fe486f86851afe6048
dir-FY25niigakimoe <moe.niigaki.a389@dirbato.co.jp>
2025-05-23 12:27:15 +0900

    宣言や代入など

---
+++ Sample/src/Sample/Variable.java
@@ -0,0 +1,13 @@
+package Sample;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        
+        String data= "何らかのデータ";
+        System.out.print(data);
+        
+        
+    }
+
+}
